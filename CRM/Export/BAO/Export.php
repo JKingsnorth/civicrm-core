9b642e5890fb2231d30cdc9bc5e7b7bd90314a2e
Coleman Watts <coleman@civicrm.org>
2014-08-06 12:59:25 +0100

    CRM-15048 - Respect custom field text_length during export

--- CRM/Export/BAO/Export.php
+++ CRM/Export/BAO/Export.php
@@ -1367,10 +1367,14 @@ INSERT INTO {$componentTable} SELECT distinct gc.contact_id FROM civicrm_group_c
           if (isset($query->_fields[$field]['data_type'])) {
 
             switch ($query->_fields[$field]['data_type']) {
+              case 'String':
+                $length = CRM_Utils_Array::value('text_length', $query->_fields[$field], 255);
+                $sqlColumns[$fieldName] = "$fieldName varchar($length)";
+                break;
+
               case 'Country':
               case 'StateProvince':
               case 'Link':
-              case 'String':
                 $sqlColumns[$fieldName] = "$fieldName varchar(255)";
                 break;
 
